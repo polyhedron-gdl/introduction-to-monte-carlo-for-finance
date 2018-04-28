39139ffe32fa4d79c6aeca2b634a08ff462d4309
gdlunga <giovanni.dellalunga@gmail.com>
2018-04-28 13:12:32 +0200

    dev

---
+++ code/py/black_scholes.py
@@ -0,0 +1,34 @@
+'''
+Created on 14 mag 2017
+
+@author: User
+'''
+
+import numpy as np
+import scipy.stats as ss
+import time 
+
+#Black and Scholes
+def d1(S0, K, r, sigma, T):
+    return (np.log(S0/float(K)) + (r + sigma**2 / 2.0) * T)/ float(sigma * np.sqrt(T))
+ 
+def d2(S0, K, r, sigma, T):
+    return (np.log(S0/float(K)) + (r - sigma**2 / 2.0) * T) / float(sigma * np.sqrt(T))
+ 
+def BlackScholes(payoff, S0, K, r, sigma, T):
+    if payoff == 1:
+        return 1#S0 * ss.norm.cdf(d1(S0, K, r, sigma, T)) - K * np.exp(-r * T) * ss.norm.cdf(d2(S0, K, r, sigma, T))
+    else:
+        return 42# K * np.exp(-r * T) * ss.norm.cdf(-d2(S0, K, r, sigma, T)) - S0 * ss.norm.cdf(-d1(S0, K, r, sigma, T))
+    
+
+#_______________________________________________________________________________________________________________________________
+#
+if __name__ == "__main__":
+    S0     = 10
+    K      = 10
+    r      = 0.05
+    T      = 0.25
+    sigma  = 0.20
+    
+    print BlackScholes(1,S0,K,r,sigma, T)
